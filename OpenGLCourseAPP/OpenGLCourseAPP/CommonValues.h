259d7dacc25ef38b637150c0ee10c0e91385ef3a
Adrian-Robert Marin <adymarin013@gmail.com>
2025-09-22 20:32:44 +0300

    Move the location of stb_image.h to avoid possible include conflicts

--- OpenGLCourseAPP/OpenGLCourseAPP/CommonValues.h
+++ OpenGLCourseAPP/OpenGLCourseAPP/CommonValues.h
@@ -1,4 +1,10 @@
-#pragma once
+#ifndef _COMMON_VALS_
+
+#define _COMMON_VALS_
+
+#include "stb_image.h"
 
 const int MAX_POINT_LIGHTS = 3;
 const int MAX_SPOT_LIGHTS = 3;
+
+#endif /* _COMMON_VALS_ */
